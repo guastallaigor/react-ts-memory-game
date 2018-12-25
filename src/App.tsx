632006cfd7eec961d0c63b04bcbca9fd37326818
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-12-25 16:48:49 -0200

    added restart and counter

--- src/App.tsx
+++ src/App.tsx
@@ -82,8 +82,29 @@ const Hourglass = styled.div`
 const Counter = styled.span`
   font-weight: bold;
   color: #fff;
-  font-size: 3em;
-  margin-right: 2em;
+  position: absolute;
+  left: 4.3em;
+  top: 2.5em;
+  font-size: 2em;
+`
+
+const StyledButton = styled.button`
+  font-size: 2em;
+  position: absolute;
+  right: 4.3em;
+  top: 1.43em;
+  font-weight: bold;
+  color: #fff;
+  background-image: linear-gradient(to top, #d4312b 65%, #fd9145);
+  width: 180px;
+  height: 80px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: opacity .2s ease-out;
+
+  &:hover {
+    opacity: .7;
+  }
 `
 
 interface IState { 
@@ -123,8 +144,9 @@ class App extends React.Component<{}, IState> {
     return (
       <MainView> 
         <Title>
-          <Counter>{counter}</Counter>
+          <Counter>Matched cards: {counter}</Counter>
           <StyledImageTitle src={require('./logo.jpg')} alt="img-title"/>
+          <StyledButton onClick={this.restart}>Restart</StyledButton>
         </Title>
         <Cards>
           {cards.map((card: ICardInterface, index: number) => (
@@ -145,21 +167,47 @@ class App extends React.Component<{}, IState> {
       let newCards: ICardInterface[] = [];
 
       if (!incrementCounter) {
-        newCards = cards.map((card: ICardInterface, index: number) => {
-          if (card1.id === card.id || card2.id === card.id) {
-            card.isTurned = false
-          }
-  
-          return card;
-        });
-
-        this.setState(() => ({ openedCards: [], cards: newCards }));
+        setTimeout(() => {
+          newCards = cards.map((card: ICardInterface, index: number) => {
+            if (card1.id === card.id || card2.id === card.id) {
+              card.isTurned = false
+            }
+    
+            return card;
+          });
+          
+          this.setState(() => ({ openedCards: [], cards: newCards }));
+        }, 1000)
 
         return;
       }
       
-      this.setState(() => ({ openedCards: [], cards: newCards, counter: counter + 1 }));
+      this.setState(() => ({ openedCards: [], counter: counter + 1 }));
     }
+  }
+
+  private getAllCardsFolded = () => {
+    const { cardsDuplicated, cards } = this.state;
+    const duplicatedCardsFolded = cardsDuplicated.map((card: ICardInterface) => {
+      card.isTurned = false;
+
+      return card;
+    });
+    const cardsFolded = cards.map((card: ICardInterface) => {
+      card.isTurned = false;
+
+      return card;
+    });
+
+    this.setState(() => ({ cardsDuplicated: duplicatedCardsFolded, cards: cardsFolded }));
+  }
+
+  private restart = () => {
+    this.getAllCardsFolded();
+    setTimeout(() => {
+      this.setState(() => ({ cards: [], counter: 0, canShow: false }));
+      this.init();
+    }, 400)
   }
 
   private shuffle = (array: any) => {
@@ -188,15 +236,23 @@ class App extends React.Component<{}, IState> {
       return data;
     }))
 
+    this.init();
+  }
+
+  private init = () => {
     const { cardsDuplicated } = this.state;
-    const otherDuplicate = cardsDuplicated.slice(0);
+    const otherDuplicate = cardsDuplicated.map((card: ICardInterface) => ({ ...card}));
     const shuffledCards = this.shuffle([...cardsDuplicated, ...otherDuplicate]);
 
     this.setState(() => ({ canShow: true, cards: [...shuffledCards] }));
   }
 
   private handleTurn = (idx: number) => {
-    const { cards } = this.state;    
+    const { cards, openedCards } = this.state;
+    if (openedCards.length === 2) {
+      return;
+    }
+
     const newCards = cards.map((card: ICardInterface, index: number) => {
       if (index === idx) {
         if (card.isTurned) {
@@ -204,7 +260,7 @@ class App extends React.Component<{}, IState> {
         }
 
         card.isTurned = !card.isTurned;
-        this.setState(() => ({ openedCards: [...this.state.openedCards, card]}));        
+        this.setState(() => ({ openedCards: [...openedCards, card]}));        
       }
 
       return card;
